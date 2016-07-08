f43354ad2d36c2613c258ab86a79f601a14cd33d
1egoman <rsg1egoman@gmail.com>
2016-07-08 19:04:20 -0400

    code: init commander

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 #!/usr/bin/env node
+import program from 'commander';
 
-export default function hello() {
-  return {
-    data: "Hello world",
-  };
-}
+program.version('0.0.1');
+
+// ----------------------------------------------------------------------------
+// biome use
+// ----------------------------------------------------------------------------
+program
+.command('use [project]')
+.action(project => {
+  console.log(req)
+});
+
+program.parse(process.argv);
