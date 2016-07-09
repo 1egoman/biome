4e64626f12a532fe602a4701078b2a86817636b8
1egoman <rsg1egoman@gmail.com>
2016-07-08 22:32:45 -0400

    code: add add task

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import program from 'commander';
 import preflight from './preflight';
 
 import use from './use';
+import add from './add';
 import init from './init';
 
-program.version('0.0.1');
+program.version(require("../package").version);
 
 // ----------------------------------------------------------------------------
 // biome init <project>
-// Open a shell containing a project's variables
+// Creatw a new project
 // ----------------------------------------------------------------------------
 program
 .command('init <project>')
@@ -18,6 +19,38 @@ program
   init(project).then(out => {
     console.log(`Created new project ${project}. Add new vars with biome add or fire it up with biome use.`);
   }).catch(console.error.bind(console));
+});
+
+// ----------------------------------------------------------------------------
+// biome add [COMMAND]=[value]
+// add an environment variable to the project's variables
+// ----------------------------------------------------------------------------
+program
+.command('add [project]')
+.description("Create a new project with the specified name, and save an alias to this folder.")
+.action(project => {
+  // if the first part was not a project, reset the variable
+  if (project.indexOf('=') !== -1) { project = undefined; }
+
+  // parse out variables
+  const matchVarRegex = /([^ ]+)\=(["'].*["']|[^ ])/gi;
+  let args = process.argv.slice(3).join(' ');
+
+  let match = matchVarRegex.exec(args);
+  if (match === null) {
+    console.error("No args were passed. Pass variables like NAME=value.")
+  } else {
+    // collect all matches
+    let allMatches = [];
+    while (match !== null) {
+      allMatches.push(match);
+      match = matchVarRegex.exec(args);
+    }
+
+    add(project, allMatches).then(out => {
+      console.log(`Sourced all variables. Try biome use to try out what you just added.`);
+    }).catch(console.log.error.bind(console))
+  }
 });
 
 // ----------------------------------------------------------------------------
