7325a3ba9ca2a01d8a3c895b9adc34c15067fa33
1egoman <rsg1egoman@gmail.com>
2016-07-08 21:30:16 -0400

    code: comments

--- src/index.js
+++ src/index.js
@@ -7,12 +7,13 @@ program.version('0.0.1');
 
 // ----------------------------------------------------------------------------
 // biome use [project]
+// Open a shell containing a project's variables
 // ----------------------------------------------------------------------------
 program
 .command('use [project]')
+.description("Open a shell with a project's associated variables included")
 .action(project => {
   use(project).catch(console.error.bind(console));
-  // console.log(project)
 });
 
 program.parse(process.argv);
