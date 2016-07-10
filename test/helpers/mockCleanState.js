a8795956534a23bfad99b53b70a71822ccf737a2
1egoman <rsg1egoman@gmail.com>
2016-07-09 20:46:28 -0400

    tests: add add task tests

---
+++ test/helpers/mockCleanState.js
@@ -0,0 +1,18 @@
+import fs from 'fs';
+import path from 'path';
+import mockFs from 'mock-fs';
+import untildify from 'untildify';
+
+export default function mockCleanSlate() {
+  let home = untildify('~/');
+  return mockFs({
+    [home]: {
+      '.biome': {
+        'project.json': '{}',
+      },
+    },
+    [process.cwd()]: {
+      'Biomefile': JSON.stringify({name: "project"}),
+    },
+  }, {createCwd: false});
+}
